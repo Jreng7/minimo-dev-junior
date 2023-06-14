d84cf3b0a2c2be2e0e7605c1162d1a1c6a2eb886
Josué R S Jacinto <jr.waydev@gmail.com>
2023-06-14 13:31:41 +0000

    routes and change server for app

--- src/app.js
+++ src/app.js
@@ -1,15 +1,22 @@
-import express from 'express';
+import express, { Router } from 'express';
 import cors from 'cors';
+// import routes from './routes';
 
 class App {
     constructor() {
-        this.server = express();
-        this.server = cors();
+        this.app = express();
+        this.middlawares();
+        this.routes();
     }
 
     middlawares(){
-        this.server.use()
+        this.app.use(express.json());
+        this.app.use(cors());
+    }
+    routes(){
+        const routes = new Router();
+        this.app.use(routes);
     }
 }
 
-export default new App().server;
+export default new App();
